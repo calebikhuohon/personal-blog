d9a97e81f19315f0be425bbd6880d6c87cecf780
Caleb Ikhuohon <calebikhuohon@gmail.com>
2020-03-01 05:31:47 +0100

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/high_res_icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
